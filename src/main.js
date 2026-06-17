e2c7b46aa2ee20e3110d1e2d295b5e3d5624fb01
htikkke <htikew574@gmail.com>
2026-06-17 14:39:57 +0630

    add js for pop up menu.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
 import "./style.css";
+
+const menuIcon = document.getElementById("menu-icon");
+const popupMenu = document.getElementById("popup-menu");
+const menuOverlay = document.getElementById("menu-overlay");
+let isMenuOpen = false;
+
+menuIcon.addEventListener("click", () => {
+  isMenuOpen = !isMenuOpen;
+  if (isMenuOpen) {
+    menuIcon.innerHTML = `<img src="/src/images/icon-close.svg" alt="closeIcon" />`;
+    menuOverlay.classList.remove("hidden");
+    popupMenu.classList.remove("hidden");
+    popupMenu.classList.add("flex");
+  } else {
+    menuIcon.innerHTML = `<img src="/src/images/icon-hamburger.svg" alt="hamburgerIcon" />`;
+    menuOverlay.classList.add("hidden");
+    popupMenu.classList.remove("flex");
+    popupMenu.classList.add("hidden");
+  }
+});
